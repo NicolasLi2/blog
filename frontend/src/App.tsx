e4cb610685b02a07e198d8088d5a5dbc145b8f63
nicolas <linze201@gmail.com>
2024-02-26 19:33:21 +0800

    create pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,38 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+  },
+  {
+    path: '/sign-up',
+    element: <SignUp />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+  },
+]);
+
 export default function App() {
-  return <div className='text-4xl'>App</div>;
+  return <RouterProvider router={router} />;
 }
