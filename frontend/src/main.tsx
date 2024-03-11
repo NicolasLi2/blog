32aa5c5fdf0e26cdb3dc1a43465e8651f64bb834
nicolas <linze201@gmail.com>
2024-03-11 12:37:23 +0800

    complete dark mode functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx';
 import './index.css';
 import { persistor, store } from './redux/store.ts';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
